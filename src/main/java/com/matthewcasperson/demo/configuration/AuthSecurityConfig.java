14850eca0d105e18049a23aac02d8d182c5ecb11
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-07 09:40:38 +1000

    Added nice homepage

---
+++ src/main/java/com/matthewcasperson/demo/configuration/AuthSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.matthewcasperson.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+@EnableWebSecurity
+@Configuration
+public class AuthSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/*.js", "/*.css").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .oauth2Login()
+                    .loginPage("/login").permitAll();
+
+    }
+}
