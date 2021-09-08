ebc12c6ca4fe567fe437c5865f05a11f22746320
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-08 13:30:08 +1000

    Get JWT

--- src/main/java/com/matthewcasperson/demo/controllers/ProfileController.java
+++ src/main/java/com/matthewcasperson/demo/controllers/ProfileController.java
@@ -3,9 +3,12 @@ package com.matthewcasperson.demo.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -14,14 +17,40 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProfileController {
+
+    private OAuth2AuthorizedClientService clientService;
+
+    @Autowired
+    public ProfileController(OAuth2AuthorizedClientService clientService) {
+        this.clientService = clientService;
+    }
+
     @GetMapping("/profile")
     public ModelAndView profile(@AuthenticationPrincipal OidcUser principal) {
         ModelAndView mav = new ModelAndView("profile");
-        mav.addObject("tokenAttributes", getUserAsJSON(principal.getAttributes()));
+        mav.addObject("tokenAttributes", getObjectAsJSON(principal.getAttributes()));
+        mav.addObject("jwt", getAccessToken());
         return mav;
     }
 
-    private String getUserAsJSON(Object attributes) {
+    private String getAccessToken() {
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+
+        OAuth2AuthenticationToken oauthToken =
+                (OAuth2AuthenticationToken) authentication;
+
+        OAuth2AuthorizedClient client =
+                clientService.loadAuthorizedClient(
+                        oauthToken.getAuthorizedClientRegistrationId(),
+                        oauthToken.getName());
+
+        return client.getAccessToken().getTokenValue();
+    }
+
+    private String getObjectAsJSON(Object attributes) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         try {
