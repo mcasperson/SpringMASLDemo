f7d4b388fc8bef405b17a1f6aefcbb4e808108eb
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-23 11:01:01 +1000

    Don't paste JWT in web page

--- src/main/java/com/matthewcasperson/demo/controllers/ProfileController.java
+++ src/main/java/com/matthewcasperson/demo/controllers/ProfileController.java
@@ -5,14 +5,7 @@ package com.matthewcasperson.demo.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,16 +14,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ProfileController {
 
-    // https://spring.io/blog/2021/01/13/the-latest-on-azure-active-directory-integration
-    // https://docs.microsoft.com/en-us/azure/developer/java/spring-framework/spring-boot-starter-for-azure-active-directory-developer-guide
     @GetMapping("/profile")
     public ModelAndView profile(
-            @AuthenticationPrincipal OidcUser principal,
-            @RegisteredOAuth2AuthorizedClient("calendar-api") OAuth2AuthorizedClient client) {
+            @AuthenticationPrincipal OidcUser principal) {
 
         ModelAndView mav = new ModelAndView("profile");
         mav.addObject("tokenAttributes", getObjectAsJSON(principal.getAttributes()));
-        mav.addObject("jwt", client.getAccessToken().getTokenValue());
         return mav;
     }
 
