9525051613a163f665cd440ba308fd843c590f86
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-23 12:09:57 +1000

    Added better date parsing

--- src/main/java/com/matthewcasperson/demo/model/GraphEventDate.java
+++ src/main/java/com/matthewcasperson/demo/model/GraphEventDate.java
@@ -1,3 +1,15 @@
 package com.matthewcasperson.demo.model;
 
-public record GraphEventDate(String dateTime, String timeZone) {}
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public record GraphEventDate(String dateTime, String timeZone) {
+    public Date getFormattedDateTime() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS", Locale.ENGLISH);
+        formatter.setTimeZone(TimeZone.getTimeZone(timeZone));
+        return formatter.parse(dateTime);
+    }
+}
