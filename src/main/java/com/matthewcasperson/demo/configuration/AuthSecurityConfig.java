e7210405ecb5de67cb44f266831652b0e0002ee3
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-25 14:02:14 +1000

    Small code cleanups

--- src/main/java/com/matthewcasperson/demo/configuration/AuthSecurityConfig.java
+++ src/main/java/com/matthewcasperson/demo/configuration/AuthSecurityConfig.java
@@ -3,12 +3,10 @@
 package com.matthewcasperson.demo.configuration;
 
 import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthSecurityConfig extends AADWebSecurityConfigurerAdapter {
 
     @Override
