c1e2ffc79f3cda854a744fd1bb8e4096a21dd022
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-25 19:18:07 +1000

    Used webclient for uploads

--- src/main/java/com/matthewcasperson/demo/controllers/UploadController.java
+++ src/main/java/com/matthewcasperson/demo/controllers/UploadController.java
@@ -2,10 +2,8 @@
 
 package com.matthewcasperson.demo.controllers;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import com.matthewcasperson.demo.model.GraphValues;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
@@ -13,9 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
 @Controller
 public class UploadController {
+
+    @Autowired
+    private WebClient webClient;
 
     @GetMapping("/upload")
     public String upload() {
@@ -35,20 +40,14 @@ public class UploadController {
     private void saveFile(OAuth2AuthorizedClient client, String fileName, String body) {
         try {
             if (null != client) {
-                System.out.println("\n" + client.getAccessToken().getTokenValue() + "\n");
-
-                RestTemplate restTemplate = new RestTemplate();
-
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.TEXT_PLAIN);
-                headers.set("Authorization", "Bearer " + client.getAccessToken().getTokenValue());
-                HttpEntity<String> entity = new HttpEntity<>(body, headers);
-
-                restTemplate.exchange(
-                        "http://localhost:8082/upload/" + fileName,
-                        HttpMethod.PUT,
-                        entity,
-                        String.class);
+                webClient
+                        .put()
+                        .uri("http://localhost:8082/upload/" + fileName)
+                        .body(BodyInserters.fromValue(body))
+                        .attributes(oauth2AuthorizedClient(client))
+                        .retrieve()
+                        .bodyToMono(GraphValues.class)
+                        .block();
             }
         } catch (Exception ex) {
             System.out.println(ex);
